4078f75e5e81bf522f8846f5b2add67da69ef7f9
Rob Henley <rob.henley@gmail.com>
2024-08-13 11:40:14 -0500

    Init

---
+++ cmd/sites/sites.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 Rob Henley <rob.henley@gmail.com>
+*/
+package sites
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// sitesCmd represents the sites command
+var SitesCmd = &cobra.Command{
+	Use:   "sites",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+}
+
+func init() {
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sitesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sitesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	SitesCmd.AddCommand(listCmd)
+	SitesCmd.AddCommand(getCmd)
+	SitesCmd.AddCommand(createCmd)
+	SitesCmd.AddCommand(updateCmd)
+	SitesCmd.AddCommand(deleteCmd)
+
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+Copyright © 2024 Rob Henley <rob.henley@gmail.com>
+
+https://wpengineapi.com/reference
+*/
+package main
+
+import "github.com/robhenley/go-wpe-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
