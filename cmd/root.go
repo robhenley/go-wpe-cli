1a3fe8a5bd137c2eb6592bde41c55ea0833775af
Rob Henley <rob.henley@gmail.com>
2024-09-19 19:48:28 -0500

    Remove unneeded comments

--- cmd/root.go
+++ cmd/root.go
@@ -28,8 +28,6 @@ var rootCmd = &cobra.Command{
 	Short: "The Unofficial WP Engine CLI",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
