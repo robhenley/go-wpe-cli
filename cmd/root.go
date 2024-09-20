ffd5784395e9aab3d16e426047d7f5feb579a92b
Rob Henley <rob.henley@gmail.com>
2024-09-19 19:48:14 -0500

    Just felt like it need the word 'The'

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "wpe",
-	Short: "Unofficial WP Engine CLI",
+	Short: "The Unofficial WP Engine CLI",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
